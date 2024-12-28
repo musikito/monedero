6a3fdcd028a30f470b7bd6ad6a0c25dcd6531b53
JoséMiguel Marte <tumusicalatina@gmail.com>
2024-12-27 20:50:50 -0500

    second

---
+++ lib/app_view.dart
@@ -0,0 +1,25 @@
+import "package:flutter/material.dart";
+
+import "home/views/home_screen.dart";
+class MyAppView extends StatelessWidget {
+  const MyAppView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Monedero",
+      theme: ThemeData(
+        colorScheme: ColorScheme.light(
+          background: Colors.grey.shade100,
+          onBackground: Colors.black,
+          primary: Color(0xFF00B2E7),
+          secondary: Color(0xFFE064f7),
+          tertiary: Color(0xFFFF8D6C),
+          outline: Colors.grey,
+        )
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
